bd4d9df76f60badabf2096426809826d9068650b
Dipak Gorfad <77386172+dipak2811@users.noreply.github.com>
2023-03-14 18:42:29 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,31 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
   mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+
+  target: "web",
+  devServer: {
+    port: "3001",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ["*",".js", ".jsx", ".json", ".ts"],
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
       }
-    ]
+    ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
